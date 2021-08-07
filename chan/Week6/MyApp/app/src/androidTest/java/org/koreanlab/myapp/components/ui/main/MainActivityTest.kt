1b74c412460ca80a96e1dba3de7dc467efb7c4eb
evan <chanjungskim@gmail.com>
2021-08-07 19:58:24 +0900

    Update MainActivityTest.kt

--- chan/Week6/MyApp/app/src/androidTest/java/org/koreanlab/myapp/components/ui/main/MainActivityTest.kt
+++ chan/Week6/MyApp/app/src/androidTest/java/org/koreanlab/myapp/components/ui/main/MainActivityTest.kt
@@ -15,6 +15,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@LargeTest
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
     private lateinit var scenario: ActivityScenario<MainActivity>
